764594b5b3eac950644949275e144bd5513f98a2
Jens Lindahl <jensctl@gmail.com>
2018-10-26 11:27:53 +0200

    Add new method for handling statics Correct mail-method

--- python/Llab_libs/mail_helper.py
+++ python/Llab_libs/mail_helper.py
@@ -2,35 +2,34 @@ import smtplib
 import re
 import os
 
-from Llab_libs.statics_helper import load_statics
+from Llab_libs.statics_helper import load_statics3
 from email.message import EmailMessage
 
 
 class LLabMailHelper:
-    __secrets_folder__ = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'secrets')
+    server = None
 
     def __init__(self):
         # load secrets
-        s =  load_statics(os.path.join(self.__secrets_folder__,'llab_mail.secret'))
-        self.mailaddr = s['mailaddr']
-        self.mailpwd = s['mailpwd']
-        self.errormessage_destination = s['errmsgdest']
-        smtpaddr = s['smtpaddr']
-        smtpport = s['smtpport']
+        self.s = load_statics3('llab_mail.secret')
 
         # setup server connection
         try:
-            server = smtplib.SMTP_SSL(smtpaddr, smtpport)
-            server.ehlo()
-            server.login(self.mailaddr, self.mailpwd)
+            self.server = smtplib.SMTP_SSL(self.s.smtpaddr + ':' + self.s.smtpport)
+            self.server.ehlo()
+            self.server.login(self.s.mailaddr, self.s.mailpwd)
         except Exception as e:
             print("Couldn't setup connection to server")
             print(e)
 
     def send_error_mail(self, sender, msg):
-        msg = EmailMessage()
-        msg['Subject'] = 'Error detected in %s' % sender
-        msg['From'] = self.mailaddr
-        msg['To'] = self.errormessage_destination
-        msg.set_content(msg)
-
+        m = sender.__str__() + '\n'
+        m += msg
+        if self.server:
+            try:
+                self.server.sendmail(self.s.mailaddr, self.s.errmsgdest, m.__str__())
+            except Exception as e:
+                print("Couldn't send message")
+                print(e)
+            finally:
+                self.server.quit()
--- python/Llab_libs/statics_helper.py
+++ python/Llab_libs/statics_helper.py
@@ -1,7 +1,17 @@
 import re
+import os
 
 
-def load_statics(path):
+__secrets_folder__ = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'secrets')
+
+
+class StaticsObject(object):
+    pass
+
+
+def load_statics_old(path):
+    # load_statics_old: Old method for handling statics. Loads path and outputs dict of statics
+
     s = {}
     with open(path, 'r') as f:
         for line in f:
@@ -9,3 +19,27 @@ def load_statics(path):
             (key, val) = line.split('=')
             s[key] = val
     return s
+
+
+def load_statics2(path):
+    # load_statics2: Newer method for handling statics. Loads path and outputs object
+    s = StaticsObject()
+    with open(path, 'r') as f:
+        for line in f:
+            line = re.sub("[ '\n]", '', line)
+            (key, val) = line.split('=')
+            setattr(s, key, val)
+    return s
+
+
+def load_statics3(name):
+    # load_statics3: Final method for handling statics. Loads name from secrets folder, outputs object
+    path = os.path.join(__secrets_folder__, name)
+    s = StaticsObject()
+    with open(path, 'r') as f:
+        for line in f:
+            line = re.sub("[ '\n]", '', line)
+            (key, val) = line.split('=')
+            setattr(s, key, val)
+    return s
+
