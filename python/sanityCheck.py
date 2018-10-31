f46b56f0f6682c5a556dcd83aea0a1ae097d81c9
Jens Lindahl <jensctl@gmail.dk>
2018-10-31 13:11:34 +0100

    PEP8

--- python/sanityCheck.py
+++ python/sanityCheck.py
@@ -4,6 +4,7 @@ import sys
 # Kaltura helpers
 from Llab_libs.KalturaExtensions import *
 
+
 def now():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -22,16 +23,15 @@ if __name__ == '__main__':
         for x1, y1 in res1.items():
             printKalturaObject(y1, specificVariables=['id', 'name'])
             res2 = client.get_entries(filters={'rootEntryIdEqual': x1,
-                                              'mediaTypeEqual': 1})
+                                               'mediaTypeEqual': 1})
             for x2, y2 in res2.items():       
                 printKalturaObject(y2, levelOfIndent=1, specificVariables=['id', 'name', 'createdAt'])                          
                 res3 = client.get_entries(filters={'parentEntryIdEqual': x2,
-                                                  'mediaTypeEqual': 1})
+                                                   'mediaTypeEqual': 1})
                 for x3, y3 in res3.items():       
                     printKalturaObject(y3, levelOfIndent=2, specificVariables=['id', 'name', 'parentEntryId', 'createdAt', 'status', 'duration', 'durationType', 'mediaType'])  
                     
                     print()
-
 
     if 'users' in sys.argv:
         print(now(), 'Get existing users')
