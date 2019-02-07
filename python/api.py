2abd25dcddf5ef9394090bd5d2057f609545aee4
Jens Lindahl <jensctl@gmail.dk>
2019-02-07 17:24:42 +0100

    Api hotfix

--- python/api.py
+++ python/api.py
@@ -44,15 +44,15 @@ if __name__ == '__main__':
             res = client.get_entries(filters={'categoriesIdsMatchAnd': args.categoryId})
             json_prep = dict()
             for key, item in res.items():
-                preprocessor = SkipFilter([], ['status',
-                                               'moderationStatus',
-                                               'type',
-                                               'licenseType',
-                                               'replacementStatus',
-                                               'operationAttributes',
-                                               'displayInSearch',
-                                               'mediaType',
-                                               'sourceType',
-                                               'searchProviderType'])
                 json_prep[key] = item.__dict__
+            preprocessor = SkipFilter([], ['status',
+                                           'moderationStatus',
+                                           'type',
+                                           'licenseType',
+                                           'replacementStatus',
+                                           'operationAttributes',
+                                           'displayInSearch',
+                                           'mediaType',
+                                           'sourceType',
+                                           'searchProviderType'])
             print(json.dumps(preprocessor.filter(json_prep)))
